1c888be55ec66ee3915f88ca41b3ab33e539fe88
Aldi <aldiksk@gmail.com>
2021-09-04 15:29:04 +0700

    New Barang Table

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -11,4 +11,5 @@ class Barang extends Model
 
     protected $table = 'barang';
     protected $fillable = ['nama_barang', 'jumlah', 'client_pt', 'nama_client', 'barang_keluar', 'sisakeluar'];
+    
 }
