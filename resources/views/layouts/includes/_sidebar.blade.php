9d28e8c819e57b5a201d5312e74a163b5c15d23e
alfan371 <alfan371@gmail.com>
2021-09-13 17:54:43 +0700

    add multi user login + registration

--- resources/views/layouts/includes/_sidebar.blade.php
+++ resources/views/layouts/includes/_sidebar.blade.php
@@ -4,7 +4,7 @@
             <br>
             <ul class="nav">
 
-                <li><a class="nav-link {{ Request::is('') ? 'active' : '' }}" aria-current="page" href="/"><i class="lnr lnr-home"></i> <span>Dashboard</span></a></li>
+                <li><a class="nav-link {{ Request::is('/') ? 'active' : '' }}" aria-current="page" href="/"><i class="lnr lnr-home"></i> <span>Dashboard</span></a></li>
                 <li><a class="nav-link {{ Request::is('barang') ? 'active' : '' }}" aria-current="page" href="/barang"><i class="lnr lnr-lighter"></i> <span>Barang</span></a></li>
                 @if (auth()->user()->level == "Karyawan Admin")
                 <li><a class="nav-link {{ Request::is('penjualan') ? 'active' : '' }}" aria-current="page" href="/penjualan"><i class="lnr lnr-lighter"></i> <span>Penjualan</span></a></li>
