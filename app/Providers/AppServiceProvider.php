0a987b6196f5e601914a90c3d662047914256aac
alfan371 <alfan371@gmail.com>
2021-09-20 16:33:26 +0700

    fix some fiture update+create user

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Auth;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        view()->composer('layouts.includes._navbar', function ($view) {
+            $view->with('user', Auth::user());
+        });
     }
 }
